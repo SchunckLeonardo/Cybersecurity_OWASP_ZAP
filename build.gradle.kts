3b457a982afc178edc4f17b23973c57af7ff9cba
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2025-10-17 09:42:40 -0300

    fix: enhance security scan workflow with improved application startup checks and vulnerability analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,10 @@ repositories {
     mavenCentral()
 }
 
+tasks.jar {
+    enabled = false
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
