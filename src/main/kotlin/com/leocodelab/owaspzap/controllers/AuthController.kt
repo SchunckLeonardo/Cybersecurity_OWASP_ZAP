f032a55b2050427e87fee8f370f204affe3d88e2
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2025-10-17 08:08:44 -0300

    feat: rename application properties to YAML and add OWASP ZAP security scan workflow

--- src/main/kotlin/com/leocodelab/owaspzap/controllers/AuthController.kt
+++ src/main/kotlin/com/leocodelab/owaspzap/controllers/AuthController.kt
@@ -2,10 +2,13 @@ package com.leocodelab.owaspzap.controllers
 
 import com.leocodelab.owaspzap.entities.AuthRequestDTO
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 val usersAllowed = mutableListOf("admin", "user1", "user2")
@@ -29,6 +32,12 @@ class AuthController {
         usersAllowed.add(authRequestDTO.username)
         passwordsAllowed.add(authRequestDTO.password)
         return ResponseEntity.status(HttpStatus.CREATED).build()
+    }
+
+    @GetMapping("/search", produces = [MediaType.TEXT_HTML_VALUE])
+    fun search(@RequestParam query: String): ResponseEntity<String> {
+        val body = "<html><body><h1>Resultado da busca por: $query</h1></body></html>"
+        return ResponseEntity.ok(body)
     }
 
 }
